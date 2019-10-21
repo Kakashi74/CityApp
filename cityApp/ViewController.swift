dfbe974d2ae5b3067a7465b78d5de41fbb2524da
Ahmed Mohamed <AhmedKaKashi74@gmail.com>
2019-10-21 14:16:04 +0200

    Initial Commit

---
+++ cityApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cityApp
+//
+//  Created by Ahmed on 10/21/19.
+//  Copyright © 2019 AhmedDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
